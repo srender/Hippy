3cb62e78e2a1b6db774e30eb0f019729b2616a42
krisye <krisye@tencent.com>
2020-09-24 19:29:17 +0800

    feat: add HippyBaseListView

--- ios/sdk/component/listview2/HippyBaseListView.h
+++ ios/sdk/component/listview2/HippyBaseListView.h
@@ -13,3 +13,28 @@
 
 @end
 
+@interface HippyBaseListView : UIView <HippyBaseListViewProtocol, HippyScrollableProtocol , UICollectionViewDelegate , UICollectionViewDataSource , HippyInvalidating>
+@property (nonatomic, copy) HippyDirectEventBlock initialListReady;
+@property (nonatomic, copy) HippyDirectEventBlock onScrollBeginDrag;
+@property (nonatomic, copy) HippyDirectEventBlock onScroll;
+@property (nonatomic, copy) HippyDirectEventBlock onScrollEndDrag;
+@property (nonatomic, copy) HippyDirectEventBlock onMomentumScrollBegin;
+@property (nonatomic, copy) HippyDirectEventBlock onMomentumScrollEnd;
+@property (nonatomic, copy) HippyDirectEventBlock onRowWillDisplay;
+@property (nonatomic, copy) HippyDirectEventBlock onEndReached;
+@property (nonatomic, assign) NSUInteger preloadItemNumber;
+@property (nonatomic, assign) CGFloat initialContentOffset;
+@property (nonatomic, assign) BOOL manualScroll;
+@property (nonatomic, assign) BOOL bounces;
+@property (nonatomic, assign) BOOL showScrollIndicator;
+
+@property (nonatomic, strong) UICollectionView *collectionView;
+@property (nonatomic, strong, readonly) HippyBaseListViewDataSource *dataSource;
+@property (nonatomic, assign) NSTimeInterval scrollEventThrottle;
+
+- (void)reloadData;
+- (Class)listViewCellClass;
+- (instancetype)initWithBridge:(HippyBridge *)bridge;
+- (void)scrollToContentOffset:(CGPoint)point animated:(BOOL)animated;
+- (void)scrollToIndex:(NSInteger)index animated:(BOOL)animated;
+@end
